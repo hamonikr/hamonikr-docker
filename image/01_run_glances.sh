f43add4623c8ccf4a849bf327ccb184f202d6d29
Kevin Kim <chaeya@gmail.com>
2021-06-14 16:07:14 +0900

    Add glances mornitoring tool

---
+++ image/01_run_glances.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup glances -w 1> /dev/null 2>&1 &
--- image/prepare.sh
+++ image/prepare.sh
@@ -47,7 +47,8 @@ wget -qO- https://pkg.hamonikr.org/add-hamonikr-5.0-hanla.apt | bash -
 $minimal_apt_get_install hamonikr-info
 $minimal_apt_get_install base-files
 $minimal_apt_get_install hamonikr-ff
-$minimal_apt_get_install glances
+$minimal_apt_get_install net-tools
+wget -qO- https://raw.githubusercontent.com/hamonikr/hamonikr-docker/master/glances/glances_install.sh | bash -
 
 # node exporter TCP/9100
 # wget -O - https://raw.githubusercontent.com/hamonikr/hamonikr-docker/master/node_exporter/agent-setup.sh | bash
